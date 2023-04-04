18c63d4796e5d219cca9c62d8fb32b615a24d2ad
Jacob David Diehl <jakediehl17@gmail.com>
2023-04-04 15:31:29 -0600

    removed build breaking config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,6 @@ Rails.application.configure do
   config.active_storage.service = :local
 
 
-  append :linked_dirs, 'storage'
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
